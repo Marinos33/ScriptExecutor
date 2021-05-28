4ce8df12b1052c6f24afe58ef048ff0bbc1de089
Loïc Midavaine <loicmidavaine@gmail.com>
2021-05-28 16:43:26 +0200

    clear

--- GameSaveBackup/GameSaveBackup/Observer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GameSaveBackup
-{
-    internal interface IObserver
-    {
-        void HandleEvent(object sender, EventArgs args); //what to do when the observable has changed
-    }
-}

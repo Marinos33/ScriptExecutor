9bceb016c63b9f530531ad0f25c76e43ed408f73
Marinos33 <loicmidavaine@gmail.com>
2022-06-29 15:28:56 +0200

    edit config

---
+++ ScriptExecutorMAUI/Services/ThreadsService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptExecutorMAUI.Services
+{
+    public class ThreadsService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (true)
+            {
+                Console.WriteLine("hello");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
